868f4d4b0c141560d450f2b0948b6cb8c7fccc95
joakim.erdfelt <joakim.erdfelt@90f627e2-05ac-11de-b321-f573fdaff03e>
2009-02-28 17:42:32 +0000

    Cleaning up redundancy in example app.

--- trunk/phxandroid-sharedprefs-one/src/org/phxandroid/sharedprefs/SharedPrefsTwoActivity.java
+++ trunk/phxandroid-sharedprefs-one/src/org/phxandroid/sharedprefs/SharedPrefsTwoActivity.java
@@ -1,25 +1,20 @@
 package org.phxandroid.sharedprefs;
 
-import java.util.Map;
-
 import android.app.Activity;
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class SharedPrefsTwoActivity extends Activity {
-    private static final String PREFERENCE_ID = "PhxAndroid";
-    public static final String  TAG           = "SharedPrefsTwo";
-    public static final int     OPT_LOAD      = 1;
-    public static final int     OPT_SAVE      = 2;
-    public static final int     OPT_DUMP      = 3;
+public class SharedPrefsTwoActivity extends Activity implements OnSharedPreferenceChangeListener {
+    private static final String TAG      = "SharedPrefsTwo";
+    public static final int     OPT_LOAD = 1;
+    public static final int     OPT_SAVE = 2;
+    public static final int     OPT_DUMP = 3;
     private EditText            input;
     private TextView            output;
 
@@ -31,6 +26,14 @@ public class SharedPrefsTwoActivity extends Activity {
 
         input = (EditText) findViewById(R.id.edit);
         output = (TextView) findViewById(R.id.out);
+
+        PrefUtil.getPrefs(this).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        PrefUtil.getPrefs(this).unregisterOnSharedPreferenceChangeListener(this);
+        super.onDestroy();
     }
 
     @Override
@@ -54,71 +57,20 @@ public class SharedPrefsTwoActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case OPT_LOAD:
-                doLoad();
+                PrefUtil.load(this, output);
                 break;
             case OPT_SAVE:
-                doSave();
+                PrefUtil.save(this, output, input.getText().toString());
                 break;
             case OPT_DUMP:
-                doDump();
+                PrefUtil.dump(this, output);
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private String userFriendly(String val) {
-        if (TextUtils.isEmpty(val)) {
-            return "<unset>";
-        }
-        return "\"" + val + "\"";
-    }
-
-    private void doLoad() {
-        Log.i(TAG, "Load");
-
-        SharedPreferences prefs = getSharedPreferences(PREFERENCE_ID, Context.MODE_PRIVATE);
-        String actid = prefs.getString("actid", "");
-        String actval = prefs.getString("actval", "");
-
-        output.append("Loaded shared preferences...\n");
-        output.append(String.format("actid = %s%n", userFriendly(actid)));
-        output.append(String.format("actval = %s%n", userFriendly(actval)));
-        output.append("Load complete\n");
-    }
-
-    private void doSave() {
-        Log.i(TAG, "Save");
-
-        SharedPreferences prefs = getSharedPreferences(PREFERENCE_ID, Context.MODE_PRIVATE);
-        Editor editor = prefs.edit();
-
-        String actid = this.getClass().getSimpleName();
-        String actval = input.getText().toString();
-
-        output.append("Saving shared preferences...\n");
-        editor.putString("actid", actid);
-        editor.putString("actval", actval);
-        output.append(String.format("actid = %s%n", userFriendly(actid)));
-        output.append(String.format("actval = %s%n", userFriendly(actval)));
-        output.append("Save complete\n");
-
-        editor.commit();
-    }
-
-    private void doDump() {
-        Log.i(TAG, "Load");
-        SharedPreferences prefs = getSharedPreferences(PREFERENCE_ID, Context.MODE_PRIVATE);
-
-        output.append("Dumping shared preferences...\n");
-        for (Map.Entry<String, ?> entry : prefs.getAll().entrySet()) {
-            Object val = entry.getValue();
-            if (val == null) {
-                output.append(String.format("%s = <null>%n", entry.getKey()));
-            } else {
-                output.append(String.format("%s = %s (%s)%n", entry.getKey(), String.valueOf(val), val.getClass()
-                        .getSimpleName()));
-            }
-        }
-        output.append("Dump complete\n");
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        Log.i(TAG, "onSharedPreferenceChanged(prefs, '" + key + "')");
     }
 }
