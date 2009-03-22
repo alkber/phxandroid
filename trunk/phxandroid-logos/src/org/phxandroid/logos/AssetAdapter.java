d5e05047d1d136d48eaccb49b3e66bb05e455941
joakim.erdfelt <joakim.erdfelt@90f627e2-05ac-11de-b321-f573fdaff03e>
2009-03-22 23:46:39 +0000

    Basic Browse / View logic in place.

--- trunk/phxandroid-logos/src/org/phxandroid/logos/AssetAdapter.java
+++ trunk/phxandroid-logos/src/org/phxandroid/logos/AssetAdapter.java
@@ -1,30 +1,41 @@
 package org.phxandroid.logos;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.util.Log;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.SpinnerAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class AssetAdapter extends BaseAdapter implements SpinnerAdapter {
-    private static final String TAG = "AssetAdapter";
-    
+public class AssetAdapter extends BaseAdapter {
+    private static final String TAG      = "AssetAdapter";
+    private static AssetAdapter INSTANCE = new AssetAdapter();
+
+    public static AssetAdapter getInstance() {
+        return INSTANCE;
+    }
+
     public class AssetRef {
+        private Bitmap icon;
         private Bitmap bitmap;
         private String name;
 
-        public AssetRef(String name, Bitmap bitmap) {
+        public AssetRef(String name, Bitmap bitmap, Bitmap icon) {
             super();
             this.name = name;
             this.bitmap = bitmap;
+            this.icon = icon;
         }
 
         public Bitmap getBitmap() {
@@ -34,46 +45,107 @@ public class AssetAdapter extends BaseAdapter implements SpinnerAdapter {
         public String getName() {
             return name;
         }
-    }
-    
-    private List<AssetRef> logos = new ArrayList<AssetRef>();
 
-    public AssetAdapter(Context context) throws IOException {
+        public Bitmap getIcon() {
+            return icon;
+        }
+    }
+
+    private List<AssetRef> logos       = new ArrayList<AssetRef>();
+    private LayoutInflater inflater;
+    private Context        context;
+    private boolean        initialized = false;
+
+    public boolean isInitialized() {
+        return initialized;
+    }
+
+    public void init(Context context) throws IOException {
+        this.context = context;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        initialized = true;
+        AssetManager assets = context.getAssets();
+
         Pattern pat = Pattern.compile("phxandroid-(.*)-logo.png");
         Matcher mat;
-        
+
         // Discover the available logos in the assets directory, load them as bitmaps, and create logos array.
-        for(String filename: context.getAssets().list("")) {
-            Log.i(TAG, "Found asset name: " + filename);
+        for (String filename : assets.list("")) {
             mat = pat.matcher(filename);
-            if(mat.matches()) {
-                
+            if (mat.matches()) {
+                InputStream in = null;
+                try {
+                    String name = Character.toUpperCase(mat.group(1).charAt(0)) + mat.group(1).substring(1);
+                    in = assets.open(filename);
+                    Bitmap bm = BitmapFactory.decodeStream(in);
+                    Bitmap icon = Bitmap.createScaledBitmap(bm, 60, 60, true);
+
+                    logos.add(new AssetRef(name, bm, icon));
+                } finally {
+                    close(in);
+                }
             }
+        }
+    }
+
+    private void close(InputStream in) {
+        if (in == null) {
+            return;
+        }
+        try {
+            in.close();
+        } catch (IOException ignore) {
+            /* ignore */
         }
     }
 
     @Override
     public int getCount() {
-        // TODO Auto-generated method stub
-        return 0;
+        return logos.size();
     }
 
     @Override
     public Object getItem(int position) {
-        // TODO Auto-generated method stub
-        return null;
+        return logos.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        // TODO Auto-generated method stub
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO Auto-generated method stub
+        View view;
+
+        if (convertView == null) {
+            view = inflater.inflate(R.layout.logo_icon, parent, false);
+        } else {
+            view = convertView;
+        }
+
+        ImageView img = (ImageView) view.findViewById(R.id.icon);
+        TextView name = (TextView) view.findViewById(R.id.name);
+
+        AssetRef ref = logos.get(position);
+        img.setImageBitmap(ref.icon);
+        name.setText(ref.name);
+        view.setTag(ref);
+
+        return view;
+    }
+
+    public AssetRef getAssetRef(String name) {
+        for (AssetRef ref : logos) {
+            if (ref.getName().equals(name)) {
+                return ref;
+            }
+        }
+
         return null;
     }
 
+    public AssetRef getAssetRef(int position) {
+        return logos.get(position);
+    }
 }
