33fc3427907966d5c2e80da69028ea97cb27256f
joakim.erdfelt <joakim.erdfelt@90f627e2-05ac-11de-b321-f573fdaff03e>
2010-10-26 04:48:18 +0000

    Adding more examples

--- networking-examples/src/org/phxandroid/examples/socket/UdpActivity.java
+++ networking-examples/src/org/phxandroid/examples/socket/UdpActivity.java
@@ -30,13 +30,13 @@ public class UdpActivity extends AbstractTestConsoleActivity {
             return;
         }
         DatagramSocket socket = null;
+        byte dummy[] = new byte[1]; // The sent packet
+        byte data[] = new byte[256]; // The received data
 
         try {
             socket = new DatagramSocket();
             socket.setSoTimeout(2000);
             socket.connect(addr);
-            byte dummy[] = new byte[1]; // The sent packet
-            byte data[] = new byte[256]; // The received data
             DatagramPacket dummyPacket = new DatagramPacket(dummy, dummy.length, addr);
             DatagramPacket datePacket = new DatagramPacket(data, data.length);
             socket.send(dummyPacket);
@@ -44,9 +44,11 @@ public class UdpActivity extends AbstractTestConsoleActivity {
             String content = new String(datePacket.getData(), datePacket.getOffset(), datePacket.getLength());
             printf("%s%n", content);
         } catch (SocketTimeoutException e) {
-            printf("Socket Timed Out attempting to communicate with %s on port %d%n", addr.getHostName(), addr.getPort());
+            printf("DatagramSocket Timed Out to %s%n", addr);
         } catch (IOException e) {
-            printf(e, "Unable to initiate DatagramSocket to host: %s (port %d)%n", addr.getHostName(), addr.getPort());
+            printf(e, "Unable to initiate DatagramSocket to host: %s%n", addr);
+        } finally {
+            close(socket);
         }
     }
 
