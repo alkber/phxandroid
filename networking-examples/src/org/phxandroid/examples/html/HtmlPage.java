f591c6a2d5b9d64310902aebd4ca78696042db04
joakim.erdfelt <joakim.erdfelt@90f627e2-05ac-11de-b321-f573fdaff03e>
2010-10-28 20:46:53 +0000

    Cleaning up Auth example

--- networking-examples/src/org/phxandroid/examples/html/HtmlPage.java
+++ networking-examples/src/org/phxandroid/examples/html/HtmlPage.java
@@ -1,11 +1,67 @@
 package org.phxandroid.examples.html;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.http.HttpEntity;
+import org.phxandroid.examples.utils.IOUtil;
 
 public class HtmlPage {
+	private List<String> links = new ArrayList<String>();
 
-    public HtmlPage(HttpEntity entity) {
-        // TODO Auto-generated constructor stub
-    }
+	public HtmlPage(HttpEntity entity) throws IOException {
+		InputStream in = null;
+		InputStreamReader reader = null;
+		BufferedReader buf = null;
+		try {
+			in = entity.getContent();
+			reader = new InputStreamReader(in);
+			buf = new BufferedReader(reader);
 
+			Pattern linkpat = Pattern.compile("href=\"([^\"]*)\"");
+			Matcher mat;
+
+			String line, link;
+			while ((line = buf.readLine()) != null) {
+				mat = linkpat.matcher(line);
+				if (mat.find()) {
+					link = mat.group(1);
+					links.add(link);
+				}
+			}
+		} finally {
+			IOUtil.close(buf);
+			IOUtil.close(reader);
+			IOUtil.close(in);
+		}
+	}
+	
+
+	@Override
+	public String toString() {
+		return "HtmlPage [links.size=" + links.size() + "]";
+	}
+
+	public List<String> getLinks() {
+		return links;
+	}
+
+	public List<String> getLinksRegex(String regex) {
+		List<String> ret = new ArrayList<String>();
+		Pattern linkpat = Pattern.compile(regex);
+
+		for (String link : links) {
+			if (linkpat.matcher(link).matches()) {
+				ret.add(link);
+			}
+		}
+
+		return ret;
+	}
 }
